2b8dcc72cbef172a75e2a0f13683c104be6923a2
ajamol2197 <ajamol2197@gmail.com>
2026-02-27 17:04:51 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,343 @@
+# 1
+def a():
+    print("salom dunyo")
+
+
+a()
+
+# 2
+
+def a():
+    print(100)
+
+
+a()
+
+# 3
+def a():
+    print("Siz muvaffaqiyat sari yurasiz!")
+
+
+a()
+
+# 4
+def a():
+    print("python " * 3)
+
+
+a()
+
+# 5
+def a():
+    print(5 + 7)
+
+
+a()
+
+# 6
+def a():
+    print("Hozir tong!")
+
+
+a()
+
+# 7
+def a():
+    print("O‘rganish - kelajak!")
+
+
+a()
+
+# 8
+def a():
+    print("Dastur ishga tushdi!")
+
+
+a()
+
+# 9
+def a():
+    print("Bugun yaxshi kun!")
+
+
+a()
+
+# 10
+def a():
+    print("Xush kelibsiz!")
+
+
+a()
+
+# 11
+def a(b):
+    print(f"salom {b}")
+
+
+a("ali")
+
+# 12
+def a(b):
+    print(b)
+
+
+a(12)
+
+# 13
+def a(v, c):
+    print(v + c)
+
+
+a(12, 8)
+
+# 14
+def v(a, b):
+    print(f"{a} {b} yoshda")
+
+
+v("bunyod", 12)
+
+# 15
+def a(b):
+    print(b*b)
+
+
+a(12)
+
+# 16
+def a(b):
+    print(b*b)
+
+
+a("asl")
+
+# 17
+def a(b, v):
+    print(b - v)
+
+
+a(12, 5)
+
+# 18
+def a(d):
+    print(f"hurmatli {d}")
+
+
+a("ali")
+
+
+# 19
+def a(n, m):
+    print(n * m)
+
+
+a(2, 3)
+
+# 20
+def a(n, m):
+    print(n * m)
+
+
+a(6, 3)
+
+# 21
+def a(s, f):
+    return s + f
+
+
+print(a(12, 3))
+
+# 22
+def f(a):
+    return a*a
+
+
+print(f(2))
+
+# 23
+def a(b):
+    return len(b)
+
+
+print(a("mali"))
+
+# 24
+def v(a, b):
+    return max(a, b)
+
+
+print(v(12, 22))
+
+# 25
+def a(s):
+    return f"Sizning matningiz: {s}"
+
+
+print(a("sli"))
+
+# 26
+def a(b, n):
+    return b - n
+
+
+print(a(12, 5))
+
+# 27
+def g(a, b):
+    return a * b
+
+
+print(g(1,  5))
+
+# 28
+def g(a, b):
+    return a / b
+
+
+print(g(10,  5))
+
+# 30
+def a(d):
+    return f"salom {d}"
+
+
+print(a("kar"))
+
+# 31
+def a(d, o="dost"):
+    return f"salom {d}"
+
+
+print(a("ali"))
+
+# 32
+def a(c, s, v=2):
+    return c ** s
+
+
+print(a(12, 4))
+
+# 33
+def a(v, b=2):
+    return v * v
+
+
+print(a(12))
+
+# 34
+def a(b, v, m=18):
+    return f"{b} {v} yoshda"
+
+
+print(a("buni", 12))
+
+# 35
+def a(c, v, o=10):
+    return c + v
+
+
+print(a(10, 10))
+
+# 36
+def a(c, v, o=2):
+    return c * v
+
+
+print(a(10, 10))
+
+# 38
+def a(v, d, o=0):
+    return v > d
+
+
+print(a(12, 3))
+
+# 39
+def matn(a, b):
+    return a + b
+
+
+print(matn(12, 5))
+
+# 40
+def ism(a):
+    return f"Xush kelibsiz: {a}"
+
+
+print(ism("ali"))
+
+# 41
+def a(e):
+    if e == 2:
+        return True
+    else:
+        return False
+
+
+print(a(5))
+
+# 42
+def raq(a, b):
+    if a == b:
+        return True
+    else:
+        return False
+
+
+print(raq(12, 12))
+
+# 43
+def a(d):
+    return d.upper()
+
+
+print(a("ali"))
+
+# 44
+def a(d):
+    return d.lover()
+
+
+print(a("ALI"))
+
+# 45
+def a(b):
+    return len(b)
+
+
+print(a("malibu"))
+
+# 46
+def m(a, b, c):
+    return a+b+c
+
+
+print(m(12,3,4))
+
+# 47
+def e(a, b):
+    return a + b / 2
+
+
+print(e(12, 4))
+
+# 48
+def a(b):
+    return len(b)
+
+
+print(a("malibu"))
+
+# 49
+def p(a, b, c):
+    return max(a, b, c)
+
+
+print(p(12, 44, 45))
+
+# 50
+def tabrik(a, b, c="Tabriklaymiz"):
+    return a, b, c
+
+a = tabrik("Bunyod", "Jigar",)
+print(a)
